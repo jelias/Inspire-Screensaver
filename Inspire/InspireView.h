87d59bbd6ebf639bb4f175a8de66e2abbe307bce
jelias <jacobeelias@me.com>
2014-01-13 20:10:45 -0600

    Initial Commit

---
+++ Inspire/InspireView.h
@@ -0,0 +1,13 @@
+//
+//  InspireView.h
+//  Inspire
+//
+//  Created by Jacob Elias on 1/13/14.
+//  Copyright (c) 2014 Jacob Elias. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface InspireView : ScreenSaverView
+
+@end
---
+++ Inspire/InspireView.m
@@ -0,0 +1,52 @@
+//
+//  InspireView.m
+//  Inspire
+//
+//  Created by Jacob Elias on 1/13/14.
+//  Copyright (c) 2014 Jacob Elias. All rights reserved.
+//
+
+#import "InspireView.h"
+
+@implementation InspireView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
